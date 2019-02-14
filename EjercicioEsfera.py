d56313462e3f1d1fb064d2e948b124e14cadbd85
Gabriela Blanco Lamela <47632388+gabyblanco@users.noreply.github.com>
2019-02-14 13:45:02 +0100

    Create EjercicioEsfera.py

---
+++ EjercicioEsfera.py
@@ -0,0 +1,6 @@
+pi=3.141592
+radio=12
+volumen=(4.0/3.0)*pi*radio**3
+#ponemos el .0 para que el programa sepa que debe dar decimales
+print ("El volumen de una esfera de radio " + str(radio)  + " es " + str(volumen))
+
